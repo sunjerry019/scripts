53e54e95546b89a27a955ad02934446e1827a425
Sun Yudong <sunjerry019@gmail.com>
2018-04-30 17:38:33 +0800

    Mount folders can now handle multiple entries and passwords

--- mount_folders/mount_folders.sh
+++ mount_folders/mount_folders.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 locked=1
+declare -A passwords
 
 while [ $locked == 1 ]; do
 	read -s -p "[gpg] Enter Passphrase: " GPGPASS
 	#set PASS "gpg -d passwords.gpg --passphrase $GPGPASS"
 	printf "\n\n"
 	printf "Decrypting passwords.gpg...\r"
-	PASS="$(echo $GPGPASS | gpg2 --pinentry-mode loopback --no-verbose -q --no-tty --batch --passphrase-fd 0 --armor --decrypt password.gpg)"
+	PASSES="$(echo $GPGPASS | gpg2 --pinentry-mode loopback --no-verbose -q --no-tty --batch --passphrase-fd 0 --armor --decrypt passwords.gpg)"
 	if [ $? -eq 0 ]; then
 		locked=0
 		printf "Decrypting passwords.gpg...Success\n\n"
@@ -16,11 +17,37 @@ while [ $locked == 1 ]; do
 	fi
 done
 
-#echo $PASS
+# echo "x:y q:w" | grep -o -E '\w+:\w+' | xargs -n1 echo | sed -r 's/:/\"]=\"/g'
+# | sed -r 's/\s+/\" [\"/g'
+# "\""   "[\""
+
+# PASSES=$(echo "$PASSES" | xargs -I @ -n1 echo @ | sed -r 's/:/\"]=\"/g' | sed -r 's/^/[\"/g' | sed -r 's/$/\"/g')
+# https://askubuntu.com/a/743875
+IFS_backup=$IFS
+
+while IFS=':' read name pw; do
+    passwords[$name]=$pw
+done <<<"$PASSES"
+
+IFS=$IFS_backup
+# 
+
 printf ":: mounting Public...\n"
 sudo mount -t cifs -o uid=1000,gid=1000,user=,pass= //192.168.1.1/Public /mnt/seagate/public/
-printf ":: mounting sunjerry019\n"
-sudo mount -t cifs -o uid=1000,gid=1000,user=sunjerry019,pass=$PASS //192.168.1.1/sunjerry019 /mnt/seagate/sunjerry019
+
+for name in "${!passwords[@]}"; do 
+	printf ":: mounting $name\n"
+	#echo "$name - ${passwords[$name]}";
+	sudo mount -t cifs -o uid=1000,gid=1000,user="$name",pass="${passwords[$name]}" "//192.168.1.1/$name" "/mnt/seagate/$name"
+done
+
+
+
+
+#printf ":: mounting sunjerry019\n"
+#sudo mount -t cifs -o uid=1000,gid=1000,user=sunjerry019,pass=$PASS //192.168.1.1/sunjerry019 /mnt/seagate/sunjerry019
+#printf ":: mounting tiansun\n"
+#sudo mount -t cifs -o uid=1000,gid=1000,user=tiansun,pass=$PASS //192.168.1.1/tiansun /mnt/seagate/tiansun
 
 #,pass=xkmndasd
 
