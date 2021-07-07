08872bf580ed12de6f2b010a785466c8254cbe95
Yudong Sun <sunjerry019@gmail.com>
2021-07-08 01:04:54 +0800

    Add ipv6 for ddns

--- silo/ddns/ddns.redacted.sh
+++ silo/ddns/ddns.redacted.sh
@@ -2,6 +2,7 @@
 # Updates IP Address in cloudflare
 
 IPv4=$(curl -sS -4 icanhazip.com)
+IPv6=$(curl -sS -6 icanhazip.com)s
 
 #   DNS INFO
 ZONE="<redacted>"
@@ -27,6 +28,23 @@ else
 	echo "Failure in update."
 	echo "Response: $RES"
 	exit 1
+fi
+
+if [[ ! -z "$IPv6" ]]; then
+    AAAAREC='{"type":"AAAA","name":"'$ENTRY'","content":"'$IPv6'","ttl":1,"proxied":false}'
+
+    RES2=$(curl -sS -X PATCH "https://api.cloudflare.com/client/v4/zones/$ZONE/dns_records/$ID" \
+        -H "Authorization: Bearer $APITOKEN" \
+        -H "Content-Type: application/json" \
+        --data $AAAAREC)
+    
+    if grep -q '"success":true' <<< "$RES2"; then
+	    echo "Successfully updated IP to $IPv6"
+    else
+	    echo "Failure in update for ipv4."
+	    echo "Response: $RES2"
+	    exit 1
+    fi
 fi
 
 # curl -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE/dns_records/$ID" \
