6390e3fff896163ee08bf7b0d20360805892b1c8
Yudong Sun <sunjerry019@gmail.com>
2022-01-02 08:18:02 +0100

    update details for volpino

--- ipaddrupdate/update-lrz.sh
+++ ipaddrupdate/update-lrz.sh
@@ -14,5 +14,5 @@ fi
 ipv4=$(ip addr show tun0 | grep inet | head -n 1 | awk '{print $2}')
 ipv6=$(ip addr show tun0 | grep inet6 | head -n 1 | awk '{print $2}')
 echo -e "$ipv4\n$ipv6" > ~/temp.ip
-scp -i ~/.ssh/dev_ed25519 ~/temp.ip yudong@ssh.yudong.dev:~/SunYD-Arch.ip
+scp -i ~/temp.ip yudong@ssh.yudong.dev:~/SunYD-Arch.ip
 rm ~/temp.ip
