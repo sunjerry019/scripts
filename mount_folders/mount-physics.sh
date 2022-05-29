e6117b4012e2699c5b9abc7632e24a7038a7fe56
Yudong Sun <sunjerry019@gmail.com>
2022-05-29 11:29:01 +0200

    change user name

--- mount_folders/mount-physics.sh
+++ mount_folders/mount-physics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/homes/Yudong.Sun /mnt/lrz/cip-home/ -o noperm,credentials=/home/sunyudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g)
+sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/homes/Yudong.Sun /mnt/lrz/cip-home/ -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g)
 
-sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/project/ag-pronin /mnt/lrz/ag-pronin/ -o noperm,credentials=/home/sunyudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g)
+sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/project/ag-pronin /mnt/lrz/ag-pronin/ -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g)
 
 # sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/ /mnt/lrz/dfsextern/ -o noperm,credentials=/home/sunyudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g)
