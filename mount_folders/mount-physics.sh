33cd0b54036c6a960610dbf246fc50efb0437dc0
Yudong Sun <sunjerry019@gmail.com>
2023-12-04 16:30:40 +0100

    mount physics now works from a conf file

--- mount_folders/mount-physics.sh
+++ mount_folders/mount-physics.sh
@@ -9,7 +9,12 @@ fi
 # https://phoenixnap.com/kb/bash-read#piping
 # https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
 filename='./physics.conf'
-cat "$filename" | tr -s "\n" | grep -v '^#' | tr -s "\t" | while read remote local; do echo "mounting $remote to $local"; done
+cat "$filename" | tr -s "\n" | grep -v '^#' | tr -s "\t" | 
+	while read remote local
+	do 
+		echo "mounting $remote to $local"
+		sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/$remote -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) $local
+	done
 
 # sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/ -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) /mnt/lrz/dfsextern
 
