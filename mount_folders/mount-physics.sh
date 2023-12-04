16e012c56714833771c9809772c7144a4b2a296c
Yudong Sun <sunjerry019@gmail.com>
2023-12-04 16:17:18 +0100

    mounting script for network shares

--- mount_folders/mount-physics.sh
+++ mount_folders/mount-physics.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/homes/Yudong.Sun /mnt/lrz/cip-home/ -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g)
+if [ ! -f ./physics.conf ]; then
+	echo "physics.conf not found!"
+	exit
+fi
 
-sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/project/ag-pronin /mnt/lrz/ag-pronin/ -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g)
+# https://stackoverflow.com/a/12488589
+# https://phoenixnap.com/kb/bash-read#piping
+# https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
+filename='./physics.conf'
+cat "$filename" | tr -s "\n" | grep -v '^#' | tr -s "\t" | while read remote local; do echo "mounting $remote to $local"; done
 
-# sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/ /mnt/lrz/dfsextern/ -o noperm,credentials=/home/sunyudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g)
+# sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/ -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) /mnt/lrz/dfsextern
+
+# sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/homes/Yudong.Sun -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) /mnt/lrz/cip-home/
+
+# sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/project/cip/2023-SS-NQP/Sun -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) /mnt/lrz/cip-home/0_modules/NQP/23_NQP_Mount/
+
+# sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/project/theorie/y/Yudong.Sun -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) /mnt/lrz/theorie-home/
