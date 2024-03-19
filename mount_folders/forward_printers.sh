3ad6d78321183d3916af2ad98abe36d519ed48ef
Yudong Sun <sunjerry019@gmail.com>
2024-03-19 14:57:39 +0100

    use config file for mounting folders

---
+++ mount_folders/forward_printers.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+ssh -f -L 6631:th-sv-cups.theorie.physik.uni-muenchen.de:631 -N -T Yudong.Sun@th-ws-740m08.theorie.physik.uni-muenchen.de
--- mount_folders/mount-physics.sh
+++ mount_folders/mount-physics.sh
@@ -9,17 +9,17 @@ fi
 # https://phoenixnap.com/kb/bash-read#piping
 # https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
 filename='./physics.conf'
-cat "$filename" | tr -s "\n" | grep -v '^#' | tr -s "\t" | 
+rootpath="dfsextern"
+mtfailed=0
+cat "$filename" | tr -s "\n" | grep -v '^#' | tr -s "\t" |
 	while read remote local
-	do 
+	do
 		echo "mounting $remote to $local"
-		sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/$remote -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) $local
+		sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/$rootpath/$remote -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) $local
+		if [ $? -ne 0 -a $mtfailed -eq 0 ]; then
+			echo "Attempting to mount on dfsroot"
+			rootpath="dfsroot"
+			mtfailed=1
+		fi
 	done
 
-# sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/ -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) /mnt/lrz/dfsextern
-
-# sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/homes/Yudong.Sun -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) /mnt/lrz/cip-home/
-
-# sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/project/cip/2023-SS-NQP/Sun -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) /mnt/lrz/cip-home/0_modules/NQP/23_NQP_Mount/
-
-# sudo mount -t cifs //z-sv-dfsroot.ad.physik.uni-muenchen.de/dfsextern/project/theorie/y/Yudong.Sun -o noperm,credentials=/home/yudong/.creds/.smbcredentials,uid=$(id -u),gid=$(id -g) /mnt/lrz/theorie-home/
